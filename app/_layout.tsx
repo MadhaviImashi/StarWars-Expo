f8924a5a6d4fbe127ac81a16f46abdc13cd19cab
MadhaviImashi <madhaviimashi@gmail.com>
2026-02-21 23:30:45 +0530

    Add SafeAreaProvider and QueryClientProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+          <Stack>
+            <Stack.Screen name="index" options={{ title: 'Star Wars Universe' }} />
+          </Stack>
+      </QueryClientProvider>
+    </SafeAreaProvider>
+  );
 }
